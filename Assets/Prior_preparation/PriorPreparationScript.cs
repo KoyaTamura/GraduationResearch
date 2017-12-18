e5cefd7af26a59f0dd4e7f834a13e1344c977e42
KoyaTamura <tamuchuchu@gmail.com>
2017-12-19 03:54:59 +0900

    commit
    add new system

--- Assets/Prior_preparation/PriorPreparationScript.cs
+++ Assets/Prior_preparation/PriorPreparationScript.cs
@@ -24,74 +24,94 @@ public class PriorPreparationScript : MonoBehaviour {
     private WebCamTexture SphereWebCam;
     private WebCamTexture MirrorWebCam;
 
+	//スフィアに貼り付ける映像のあれ
+	public VideoPlayer SphereMovie;
+	public VideoPlayer MirrorMovie;
+
     //その他の変数
     public Quaternion direction;
-    private bool isHead = false;
-    private bool canAlf = true;
+    private bool isUpHead = false;
+	private bool isAlf = false;
+	private bool isChange = false;
+
     public float SphereAlf = 1f;
     private Material SphereMat;
     private Material MirrorMat;
 
     // Use this for initialization
     void Start () {
-        WebCamDevice[] devices = WebCamTexture.devices;
-        if (devices.Length > SphereCamNum)
-        {
-            SphereWebCam = new WebCamTexture(devices[SphereCamNum].name, width, height, fps);
-            SphereMat = LiveSphere.GetComponent<Renderer>().material;
-            LiveSphere.GetComponent<Renderer>().material.mainTexture = SphereWebCam;
-            SphereWebCam.Play();
-
-            MirrorWebCam = new WebCamTexture(devices[MirrorCamNum].name, width, height, fps);
-            MirrorMat = LiveMirror.GetComponent<Renderer>().material;
-            LiveMirror.GetComponent<Renderer>().material.mainTexture = MirrorWebCam;
-            MirrorWebCam.Play();
-        }
-        else
-        {
-            Debug.Log("no camera");
-        }
-
-        for (int i = 0; i < devices.Length; i++)
-        {
-            Debug.Log(devices[i].name);
-        }
-
-        //Oculusの映像をうつさないようにする設定(ライブの映像には関係ない)
-        VRSettings.showDeviceView = false;
+		webCamInit ();
+		movieInit ();
+		VRSettings.showDeviceView = false; //Oculusの映像をうつさないようにする設定(ライブの映像には関係ない)
     }
 	
 
-
 	// Update is called once per frame
 	void Update () {
-        keyCont();
-
-        direction = InputTracking.GetLocalRotation(VRNode.Head);
-
-        //上を向いたタイミングでLiveSphereを消す
-        if (0.4f <= direction[0] && direction[0] < 0.50f)
-        {
-            if (canAlf)
-            {
-                isHead = true;
-            }
-            canAlf = false;
-        }
-        else {
-            canAlf = true;
-        }
-
-        if (isHead)
-        {
-            DOTween.To(() => SphereAlf, x => SphereAlf = x, 0, 2f);
-            isHead = false;
-         }
-
-        SphereMat.SetFloat("_SphereAlf", SphereAlf);
+        keyCont(); //key操作の関数
+		ControlHMD(); //HMDが上を向いた時とかの関数
     }
 
+	void movieInit(){
+		SphereMovie.Play ();
+		MirrorMovie.Play ();
+		SphereMovie.Pause ();
+		MirrorMovie.Pause ();
+	}
 
+	void webCamInit(){
+		WebCamDevice[] devices = WebCamTexture.devices;
+		if (devices.Length > SphereCamNum)
+		{
+			SphereWebCam = new WebCamTexture(devices[SphereCamNum].name, width, height, fps);
+			SphereMat = LiveSphere.GetComponent<Renderer>().material;
+			LiveSphere.GetComponent<Renderer>().material.mainTexture = SphereWebCam;
+			SphereWebCam.Play();
+
+			MirrorWebCam = new WebCamTexture(devices[MirrorCamNum].name, width, height, fps);
+			MirrorMat = LiveMirror.GetComponent<Renderer>().material;
+			LiveMirror.GetComponent<Renderer>().material.mainTexture = MirrorWebCam;
+			MirrorWebCam.Play();
+		}
+		else
+		{
+			Debug.Log("no camera");
+		}
+
+		for (int i = 0; i < devices.Length; i++)
+		{
+			Debug.Log(devices[i].name);
+		}
+	}
+
+	void ControlHMD(){
+		//上を向いたタイミングでLiveSphereを消す
+		direction = InputTracking.GetLocalRotation(VRNode.Head);
+		if (0.4f <= direction [0] && direction [0] < 0.50f) {
+			isUpHead = true;
+			if(isAlf == false && isChange){
+				DOTween.To(() => SphereAlf, x => SphereAlf = x, 0, 2f);
+				LiveMirror.SetActive (false);
+				SphereMovie.Play ();
+				MirrorMovie.Play ();
+				isAlf = true;
+			}else if(isAlf == true && isChange){
+				DOTween.To(() => SphereAlf, x => SphereAlf = x, 1, 2f);
+				LiveMirror.SetActive (true);
+				SphereMovie.Pause ();
+				MirrorMovie.Pause ();
+				isAlf = false;
+			}
+			isChange = false;
+		} else {
+			if (isUpHead) {
+				StartCoroutine ("StopAlfChange");
+			}
+			isUpHead = false;
+		}
+
+		SphereMat.SetFloat("_SphereAlf", SphereAlf);
+	}
 
     void keyCont() {
         if (Input.GetKey(KeyCode.RightArrow))
@@ -135,9 +155,16 @@ public class PriorPreparationScript : MonoBehaviour {
             LiveMirror.transform.Rotate(new Vector3(1f, 0f, 0f));
 
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D))
         {
             LiveMirror.transform.Rotate(new Vector3(-1f, 0f, 0f));
-        }
+		}
+			
     }
+
+	//アルファチェンジの微調整
+	IEnumerator StopAlfChange(){
+		yield return new WaitForSeconds (2f);
+		isChange = true;
+	}
 }
