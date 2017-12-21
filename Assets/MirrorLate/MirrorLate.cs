dfe179029abe3341382abd160c6ba5f19b9b44de
KoyaTamura <tamuchuchu@gmail.com>
2017-12-21 22:02:19 +0900

    fixed cord
    コードの修正

--- Assets/MirrorLate/MirrorLate.cs
+++ Assets/MirrorLate/MirrorLate.cs
@@ -17,18 +17,17 @@ public class MirrorLate : MonoBehaviour
 
     //代入する云々
     private Texture2D setTexture;
-    private Texture2DArray textureArray;
-    private bool webcamIsPlaying = true;
+    private bool webcamIsPlaying = false;
+
+    private float lateTime;
 
     //何秒遅らせるか
     public int lateSec = 3;
-    public float lateSpeed = 0.01f;
+    public float lateSpeed = 15.0f;
 
     //遅延を減らすスイッチ
-    private bool realTime = false;
     public bool swichLate = true;
-    private int GoBack;
-    private float fixTime;
+    private bool isLate = false;
 
     //ミラーを選択して放り込む
     public GameObject Mirror;
@@ -39,7 +38,7 @@ public class MirrorLate : MonoBehaviour
     {
 
         WebCamInit();
-        Application.targetFrameRate = fps-10; //FPS設定
+        //Application.targetFrameRate = fps - 10; //FPS設定
 
     }
 
@@ -50,51 +49,55 @@ public class MirrorLate : MonoBehaviour
         keyCont();
 
         //最初の一回で代入先のtextureを初期化
-        if (webcamTexture.width > 16 && webcamTexture.height > 16 && webcamIsPlaying)
+        if (webcamTexture.width > 16 && webcamTexture.height > 16 && !webcamIsPlaying)
         {
             SetTextureInit();
-            webcamIsPlaying = false;
+            webcamIsPlaying = true;
         }
 
-        //全て初期化されたらここに入る
-        if (webcamIsPlaying == false)
+        if (webcamIsPlaying)
         {
-
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                realTime = true;
-                GoBack = 1 - GoBack;
+                isLate = !isLate;
+                if (isLate)
+                {
+                    DOTween.To(() => lateTime, x => lateTime = x, 0.0f, lateSpeed);
+                }
+                else
+                {
+                    DOTween.To(() => lateTime, x => lateTime = x, lateSec, lateSpeed);
+                }
             }
-
-            textureArray.SetPixels(webcamTexture.GetPixels(), Time.frameCount % (lateSec * fps));
-            StartCoroutine("realTimeMovie", Time.frameCount % (fps * lateSec));
-
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                DOTween.KillAll();
+            }
+            Debug.Log(lateTime);
+            StartCoroutine(MovieLateCoroutine(lateTime));
         }
 
-        Debug.Log(fixTime);
 
     }
 
     void WebCamInit()
     {
-        //遅延した状態で始めるならGoBackは0でfixTimeは0
-        //リアルタイムで始める場合はGoBackは1でfixTimeはlateSec
+        //遅延した状態で始めるならGoBackはfalseでfixTimeは0
+        //リアルタイムで始める場合はGoBackはtrueでfixTimeはlateSec
         if (swichLate)
         {
-            GoBack = 0;
-            fixTime = 0;
+            isLate = false;
         }
         else
         {
-            GoBack = 1;
-            fixTime = lateSec;
+            isLate = true;
         }
 
         //webcamをスタートさせる
         WebCamDevice[] devices = WebCamTexture.devices;
         if (devices.Length > camNum)
         {
-            webcamTexture = new WebCamTexture(devices[camNum].name, width, height, 15);
+            webcamTexture = new WebCamTexture(devices[camNum].name, width, height, fps);
             webcamTexture.Play();
         }
         else
@@ -111,45 +114,23 @@ public class MirrorLate : MonoBehaviour
 
     void SetTextureInit()
     {
-
         //テクスチャー配列、カラー、表示テクスチャーの初期化
-        textureArray = new Texture2DArray(webcamTexture.width, webcamTexture.height, lateSec * fps, TextureFormat.RGB565, false);
         setTexture = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.RGB565, false);
         GetComponent<Renderer>().material.mainTexture = setTexture;
     }
 
-    IEnumerator realTimeMovie(int i)
+
+    IEnumerator MovieLateCoroutine(float time)
     {
+        Color[] bufferPixels = new Color[webcamTexture.width * webcamTexture.height];
+        bufferPixels = webcamTexture.GetPixels();
 
-        if (realTime && GoBack == 1)
-        {
-            if (lateSec - fixTime > 0)
-            {
-                fixTime += lateSpeed;
-            }
-            else
-            {
-                fixTime = lateSec;
-                realTime = false;
-            }
-        }
-        else if (realTime && GoBack == 0)
-        {
-            if (fixTime > 0)
-            {
-                fixTime -= lateSpeed;
-            }
-            else
-            {
-                fixTime = 0;
-                realTime = false;
-            }
-        }
+        yield return new WaitForSeconds(time);
 
-        yield return new WaitForSeconds((float)lateSec - fixTime);
-        setTexture.SetPixels(textureArray.GetPixels(i));
+        setTexture.SetPixels(bufferPixels);
         setTexture.Apply();
     }
+
     void keyCont()
     {
         if (Input.GetKey(KeyCode.RightArrow))
